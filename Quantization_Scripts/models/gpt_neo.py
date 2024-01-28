059c8182887e3e6d57bca4c6f9844c97a7bb9a3a
kellyyyang <kelyang@hmc.edu>
2024-01-28 23:04:18 +0000

    initial commit

---
+++ Quantization_Scripts/models/gpt_neo.py
@@ -0,0 +1,4 @@
+from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+
+model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
+tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
