62b66608871dc2e6ee160c3f0a755648afb5fc72
kellyyyang <kelyang@hmc.edu>
2024-02-01 09:38:44 +0000

    added some files

--- Quantization_Scripts/models/gpt_neo.py
+++ Quantization_Scripts/models/gpt_neo.py
@@ -1,4 +1,21 @@
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+# from datasets import load_dataset
+import torch
+import torch.nn.functional as F
 
-model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+print(torch.__version__)
+
+from aimet_torch.model_preparer import prepare_model
+
+
+
+# dataset = load_dataset("EleutherAI/pile")
+
+model_name = "EleutherAI/gpt-neo-1.3B"
+
+model = GPTNeoForCausalLM.from_pretrained(model_name)
+# model = prepare_model(model)
+
+# tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+print(model)
